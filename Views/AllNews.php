620af4c54b41e2e842ff1ec0539d22171ce90a4e
user <Global@mail.ua>
2019-04-13 11:52:39 +0300

    AllNews.php new version

--- Views/AllNews.php
+++ Views/AllNews.php
@@ -1,11 +1,18 @@
 <input type="submit" action='Autorization.php' name="index_goToLogin" value="Войти" />
 <div id="index_allNewsDiv">
+<?php
+	foreach ($variable as $key => $value) {
+		# code...
+	}
+?>
+<!-->foreach form database
 	<div name="news_id">
-			<div>
-				<a href="SingleNews.php">Название новости</a>
+				<div>
+					<a href="SingleNews.php">Название новости</a>
+				</div>
 				<div>
 					<p>Some text not more 100 symbols</p>
 				</div>
-			</div>
 	</div>
+	<-->
 </div>

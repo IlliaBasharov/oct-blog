7920f7fc8de5fd110b71ce4ad8e91b0bed42bb11
introqt <introtheintro@gmail.com>
2019-04-13 11:27:55 +0300

    change Authorization

--- Views/Autorization.php
+++ Views/Autorization.php
@@ -1,6 +1,16 @@
-<?php
-
-<html>
-<head></head>
-<main></main>
-</html>
+<form method="post" action="/login/">
+    <table>
+        <tr>
+            <td><label for="loginField">Логин</label></td>
+            <td><input id="loginField" type="text" name="login"></td>
+        </tr>
+        <tr>
+            <td><label for="passwordField">Пароль</label></td>
+            <td><input id="passwordField" type="password" name="password"></td>
+        </tr>
+        <tr>
+            <td colspan="2" style="text-align: center"><input type="submit" value="Войти"></td>
+			<td colspan="2" style="text-align: center"><input type="button" value="Зарегистрироваться"></td>
+        </tr>
+    </table>
+</form>

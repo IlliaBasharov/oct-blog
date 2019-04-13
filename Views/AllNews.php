11fbd0f2b4f1adef48096ac582cbe8a0cd025d74
user <Global@mail.ua>
2019-04-13 11:47:28 +0300

    AllNews.php upload

---
+++ Views/AllNews.php
@@ -0,0 +1,11 @@
+<input type="submit" action='Autorization.php' name="index_goToLogin" value="Войти" />
+<div id="index_allNewsDiv">
+	<div name="news_id">
+			<div>
+				<a href="SingleNews.php">Название новости</a>
+				<div>
+					<p>Some text not more 100 symbols</p>
+				</div>
+			</div>
+	</div>
+</div>

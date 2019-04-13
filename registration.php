9a42e2bd8288ceb6b37e7a02c832a9efdb9e0616
Vasyl <admin@gmail.com>
2019-04-13 11:59:16 +0300

    add form

--- registration.php
+++ registration.php
@@ -1,8 +1,45 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<style>
+	    #form{
+		margin-top: 50px;
+		margin-left: auto;
+		margin-right: auto;
+		width: 300px; 
+	    }
+	</style>
+	<script src="../js/main.js" type="text/javascript"></script>
+    </head>
+    <body>
+	<div id="form" class="w3-card-4">
+	    <div class="w3-container w3-teal">
+		<h2>Registration</h2>
+	    </div>
+	    <form action="/registration" class="w3-container" method="POST" name="register">
+		<p>
+		    <label class="w3-text-teal"><b>Login</b></label>
+		    <input class="w3-input w3-border w3-light-grey" type="text" name="login" required>
+		</p>
+		<p>
+		    <label class="w3-text-teal"><b>Password</b></label>
+		    <input class="w3-input w3-border w3-light-grey" type="text" name="password" required>
+		</p>
+		<p>
+		    <label class="w3-text-teal"><b>Confirm password</b></label>
+		    <input class="w3-input w3-border w3-light-grey" type="password" name="confirm_password" required>
+		</p>
+		<p>
+		    <label class="w3-text-teal"><b>Email</b></label>
+		    <input class="w3-input w3-border w3-light-grey" type="email" name="email" required>
+		</p>
+		<p>
+		    <button type="submit" class="w3-btn w3-blue-grey" name="register" value="register">Register</button>
+		</p>
+	    </form>
+	</div>
+    </body>
+</html>

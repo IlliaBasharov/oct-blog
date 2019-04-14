504c46dab3b5a9562765c9c1c942d6d2233eb5ed
maksymkornilenko <kornilenko1993@gmail.com>
2019-04-14 20:46:00 +0200

    link parameters for href to singleNews.php

--- Views/AllNews.php
+++ Views/AllNews.php
@@ -10,10 +10,11 @@
 		<div id="index_allNewsDiv">
 			<?php
 			$news_array=mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+                        $link='Views'.DIRECTORY_SEPARATOR.'SingleNews.php';
 			foreach ($news_array as $news):
 			?>
 				<div>
-					<a href='<?=$link?>'><?=$news['head']?></a><!--TODO $link=ссылка на singleNews-->
+					<a href='<?=$link?>'><?=$news['head']?></a><!--TODO $link=singleNews-->
 				</div>
 				<div>
 					<p><?=$news['text']?></p>

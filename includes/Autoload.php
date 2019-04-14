659cb8ad5d76ac15a68533c72e50dfbff1738bdc
Vadym <sobolvadim22@i.ua>
2019-04-14 15:32:53 +0300

    Autoload.php

---
+++ includes/Autoload.php
@@ -0,0 +1,8 @@
+spl_autoload_register(function($app){
+    $path = 'application'.DIRECTORY_SEPARATOR.$app.'.php';
+    if(file_exists($path)){
+	include_once $path;
+	return true;
+    }
+    return false;
+});

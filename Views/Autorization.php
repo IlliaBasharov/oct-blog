b1b408de9ddf185fd95abaca525ea93a92410345
AntonOlieinikov <oleynikovanton321@gmail.com>
2019-04-15 18:05:53 +0300

    123

--- Views/Autorization.php
+++ Views/Autorization.php
@@ -1,19 +1,3 @@
 <form method="post" action="/login/">
-    <table>
-        <tr>
-            <td><label for="loginField">Логин</label></td>
-            <td><input id="loginField" type="text" name="login"></td>
-        </tr>
-        <tr>
-            <td><label for="passwordField">Пароль</label></td>
-            <td><input id="passwordField" type="password" name="password"></td>
-        </tr>
-        <tr>
-            <td colspan="2" style="text-align: center"><input type="submit" value="Войти"></td>
-			<td colspan="2" style="text-align: center"><input type="button" value="Зарегистрироваться"></td>
-        </tr>
-		fsddsgdfsh
-		123
-    </table>
-	asdasdasd
+ <label>Enter your login:</label>
 </form>

407fdb08325922dbf160688c432dc9d264c05023
AleksandrPetrov <sckrudg@gmail.com>
2019-04-16 00:25:05 +0300

    add test

--- Views/Cabinet.php
+++ Views/Cabinet.php
@@ -1,11 +1,48 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <button type="button" name="cabinet_showNews" value="<?php $loginUser; ?>"></button>
+        <div id="cabinet_allNews" style="visibility: hidden">
+            <form action="newNews.php">
+                <?php
+                foreach ($news as $value) {
+                    echo $value;
+                }
+                ?>
+                <input type="button" name="cabinet_newNews"/>
+            </form>
+
+            <form name="news_id" method="POST">
+                <a href="singleNews.php">
+                    <input type="button" name="name_news" value="<?php $nameNewsId; ?>"/>  
+                </a>
+                <a style="visibility: hidden">
+                    <button name="delete_news"><img src="https://cdn.pixabay.com/photo/2012/04/13/00/21/button-31222_960_720.png" alt="Крестик" style="width: 12px"></button> 
+                    <input type="hidden" name="deleteNewsId" value="<?php $nameNewsId; ?>"/>
+                </a>
+                <div>
+                    <?php $textNewsId; ?>
+                </div>
+
+            </form>
+
+            <form action="newNews.php">
+                <input type="button" name="cabinet_newNews"/>
+            </form>
 
 
-foreach ($loginUser as $value) {
-    echo $value;
-}
+        </div>
+        <input type="submit" name="cabinet_logOut" value="Выход"/>
+        
+    </body>
+</html>
 
-foreach ($news as $value) {
-    echo $value;
-}
+
+
+
+
 
--- includes/cabinet.php
+++ includes/cabinet.php
@@ -1,6 +1,17 @@
 <?php
 
-$news = getNewsByLoginId($loginUser);
+$db = new DataBase(); 
 
-include_once 'Views' . DIRECTORY_SEPARATOR . 'Cabinet.php';
-
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method === 'POST') {
+    $deleteNewsId  = POST["deleteNewsId"];
+    if(!empty($deleteNewsId)){
+        $db->deletNews($deleteNewsId);
+    } else {
+        $user=new User();
+        $user->logout();
+    }
+} else if ($method === 'GET') {
+    $news = $db->getNewsByLogin($loginUser);
+    include_once 'Views' . DIRECTORY_SEPARATOR . 'Cabinet.php';
+}

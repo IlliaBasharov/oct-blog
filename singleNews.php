e405c5667fc6f8c8af0b7c5728da57798c88a0ec
DmitriyLishikov <pain14141919@gmail.com>
2019-04-15 18:09:33 +0300

    singleNews.php

--- singleNews.php
+++ singleNews.php
@@ -1,3 +1,19 @@
 <?php
-use Views\SingleNews;
 
+include_once 'includes/Autoload.php';
+
+if (isset($_GET['newsId'])) {
+    if (!empty($_GET["newsId"])) {
+        $news_arr = new DataBase();
+        $news = $news_arr->getNewsById($id);
+        if ($_GET['newsId'] === $news['id']) {
+            $view = '<h2>' . $news['name'] . '</h2><p>' . $news['text'] . '</p>' .
+                    '<img src="image.php?id=' . $news['photo'] . '" />';
+            echo $view;
+        } else {
+            echo 'Такой новости не существует';
+        }
+    } else {
+        echo 'Извините, возникла проблема в работе сайта.';
+    }
+}

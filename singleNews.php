dab4e7196bc1e9d2c953728ca031a160aeaadede
DmitriyLishikov <pain14141919@gmail.com>
2019-04-15 20:24:20 +0300

    complete singleNews.php

--- singleNews.php
+++ singleNews.php
@@ -4,8 +4,8 @@ include_once 'includes/Autoload.php';
 
 if (isset($_GET['newsId'])) {
     if (!empty($_GET["newsId"])) {
-        $news_arr = new DataBase();
-        $news = $news_arr->getNewsById($id);
+        $db = new DataBase();
+        $news = $db->getNewsById($id);
         if ($_GET['newsId'] === $news['id']) {
             $view = '<h2>' . $news['name'] . '</h2><p>' . $news['text'] . '</p>' .
                     '<img src="image.php?id=' . $news['photo'] . '" />';

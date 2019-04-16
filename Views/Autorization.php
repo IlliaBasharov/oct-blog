cdba960c6a426c6c15c75d58b7601c0cbc77035b
AntonOlieinikov <oleynikovanton321@gmail.com>
2019-04-16 09:15:11 +0300

    4321

--- Views/Autorization.php
+++ Views/Autorization.php
@@ -7,6 +7,6 @@
             <input id="passwordField" type="password" name="password" required>
        
             <input type="submit" value="Войти">
-            <input type="button" value="Зарегистрироваться" onclick="location.href=' http://oct-blog/registration.php'"></td>
+            <input type="button" value="Зарегистрироваться" onclick="location.href=' http://oct-blog/registration.php'">
         
 </form>

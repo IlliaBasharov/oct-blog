d806b7efca72fe3345b359971839c2f52e134eac
Dmytro_Yermolaiev <iermolaiev1@gmail.com>
2019-04-17 22:37:08 +0300

    сф

--- Views/Cabinet.php
+++ Views/Cabinet.php
@@ -5,7 +5,7 @@
         <title></title>
     </head>
     <body>
-        <?php var_dump($easfaf)?>
+        <?php?>
         <button type="button" name="cabinet_showNews" value="<?php $loginUser; ?>"></button>
         <div id="cabinet_allNews" style="visibility: hidden">
             <form action="newNews.php">

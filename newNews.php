6b436aca59d38882e04da89f8e62390544f54093
smirnovDm <smirnov.dm31@gmail.com>
2019-04-18 12:23:02 +0300

    init

--- newNews.php
+++ newNews.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once '..' . DIRECTORY_SEPARATOR . 'config_example.php';
+include_once '..' . DIRECTORY_SEPARATOR . 'config.php';
 include_once 'application' . DIRECTORY_SEPARATOR . 'DataBase.php';
 
 class newNews {
@@ -17,9 +17,8 @@ class newNews {
         $this->title = filter_input(INPUT_POST, 'title');
         $this->text = filter_input(INPUT_POST, 'content');
         $this->date_time = filter_input(INPUT_POST, 'date');
-        $date_components = explode('T', $this->date_time);
         $today = date('H:m:s');
-        $this->date_time = $date_components[0] . ' ' . $today;
+        $this->date_time = $this->date_time . ' ' . $today;
         $this->date_change = $this->date_time;
     }
 
@@ -73,9 +72,12 @@ class newNews {
             unset($_SESSION["file_error_message"]);
             $_SESSION["file_error_message"] = $message;
             if (empty($_SESSION['file_error_message']) && $this->validator($this->title, $this->text) ) {
-                $db = new DataBase();                                                                                //начинаем тащить в базу
-                $db->setNews($this->title, $this->text, $this->image_path, $this->date_time, $this->date_change, 3); //TODO USER_ID где взять?
-                //==================================================================================================================================
+                $login = $_SESSION['loginUser'];
+                $db = new DataBase();
+                $user = $db->getUser($login);
+                $this->user_id = $user['user_id'];
+                $db->setNews($this->title, $this->text, $this->image_path, $this->date_time, $this->date_change, $this->user_id);
+
                 header('Location: http://oct-blog/Views/AllNews.php');
             } else {
                 header('Location: http://oct-blog/Views/NewNews.php');

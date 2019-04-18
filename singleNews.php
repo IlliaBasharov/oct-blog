c2deed3c9189f116b0a3dfbea0c2211830275785
Dmytro_Yermolaiev <iermolaiev1@gmail.com>
2019-04-18 10:51:40 +0300

    Update singleNews.php

--- singleNews.php
+++ singleNews.php
@@ -2,16 +2,20 @@
 include_once 'includes/Autoload.php';
 if (isset($_GET['newsId'])) {
     if (!empty($_GET["newsId"])) {
+        //добавлена след. строка
+        $id = $_GET["newsId"];
         $db = new DataBase();
         $news = $db->getNewsById($id);
         if ($_GET['newsId'] === $news['id']) {
             $view = '<h2>' . $news['name'] . '</h2><p>' . $news['text'] . '</p>' .
-                    '<img src="image.php?id=' . $news['photo'] . '" />';
+                    '<img src="' . $news['photo'] . '" />';  //'<img src="image.php?id=' . $news['photo'] . '" />';
             echo $view;
         } else {
             echo 'Такой новости не существует';
         }
     } else {
         echo 'Извините, возникла проблема в работе сайта.';
-    }
+    }    
 }
+echo '<input type="submit" action="\index.php"/>'; // строка дописана
+

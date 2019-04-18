56c753047da2f11d5020a10e5d1edb579b5feecb
AleksandrPetrov <sckrudg@gmail.com>
2019-04-18 12:56:14 +0300

    add

--- Views/Cabinet.php
+++ Views/Cabinet.php
@@ -1,4 +1,4 @@
-<?php if (0 === 1) { ?>﻿
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -7,7 +7,6 @@
         <title></title>
     </head>
     <body>
-        <form id="allNewsUser">
             <button type="button" name="cabinet_showNews" id="log">
                 <?php echo $loginUser; ?>
             </button>
@@ -17,20 +16,18 @@
                         <input type="button" name="cabinet_newNews" value="Добавить новость" class="addNews"/>
                     </a>  
                 </p>
-                <!--            TODO пункт 2-->
-                <form name="news_id" method="POST">
                     <?php
                     $db = new DataBase();
-                    $news_array = $db->getNews();
-                    $link = 'Views' . DIRECTORY_SEPARATOR . 'SingleNews.php';
+                    $user = $db->getUser($loginUser);
+                    $news_array = $db->getNewsByLogin($loginUser);
                     foreach ($news_array as $news):
                         ?>
-                        <a href='<?= $link ?>'>
+                        <a href='<?= $link ?>.$link = "Views" . DIRECTORY_SEPARATOR . "SingleNews.php".$link.getNewsId = <?= $news[id] ?>;'>
                             <h3><?= $news['name'] ?></h3>
-                            <p><?= $news['text'] ?></p>
                         </a>
+                <p><?= $news['text'] ?></p>
                     <?php endforeach; ?>
-                </form>
+                
                 <p>
                     <a href="Views/newNews.php">
                         <input type="button" name="cabinet_newNews" value="Добавить новость" class="addNews"/>
@@ -38,27 +35,5 @@
                 </p>
             </div>
             <input type="submit" name="cabinet_logOut" value="Выход" id="output" form="allNewsUser"/>
-        </form>
     </body>
 </html>
-<?php } else { ?>
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <meta charset="UTF-8">
-            <title></title>
-        </head>
-        <body>
-            <button type="button" name="cabinet_showNews">
-                <?= $loginUser ?>
-            </button>
-            <div id = cabinet_allNews>
-                <?php
-                foreach ($news as $news_one) {
-                    
-                }
-                ?>
-            </div>
-        </body>
-    </html>
-<?php } ?>

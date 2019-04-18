7d39e150989d0c6db068c0afa80e88ba7b88a85b
DmitriyLishikov <pain14141919@gmail.com>
2019-04-18 13:36:07 +0300

    style

--- Views/SingleNews.php
+++ Views/SingleNews.php
@@ -3,6 +3,24 @@
     <head>
         <meta charset="UTF-8">
         <title></title>   
+        <style>
+            form{
+                margin: 100px auto;
+                width:300px;
+                padding:10px;
+                display:flex;
+                flex-direction: column;
+                border:1px solid black;
+                border-radius:10px;
+            }
+            input{
+                margin:5px;
+                width: 95%; 
+            }
+            form h2{
+                text-align:center;
+            }
+        </style>
     </head>
     <body>      
             <?php include_once '../singleNews.php'; ?> 

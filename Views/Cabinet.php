32d6d493e8988c1ddbd41a2e6db267d17f510a3e
Dmytro_Yermolaiev <iermolaiev1@gmail.com>
2019-04-19 22:44:40 +0300

    sfd

--- Views/Cabinet.php
+++ Views/Cabinet.php
@@ -1,51 +1,56 @@
-﻿<!DOCTYPE html>
+
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <link href="../css/Cabinet.css" rel="stylesheet" type="text/css"/>
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-	<script src="../js/main.js" type="text/javascript"></script>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <title></title>
     </head>
     <body>
-	
-	    <button type="button" name="cabinet_showNews" id="log">
-		<?php echo $loginUser; ?>
-	    </button>
-	    <div id="cabinet_allNews">
-		<p>
-		    <a href="Views/newNews.php">
-			<input type="button" name="cabinet_newNews" value="Добавить новость" class="addNews"/>
-		    </a>  
-		</p>
-		<?php
-		$db = new DataBase();
-		$user = $db->getUser($loginUser);
-		$news_array = array_reverse($db->getNewsByLogin($loginUser));
-		foreach ($news_array as $news):
-		    $link = 'http://oct-blog/SingleNews.php?newsId=' . $news['id'];
-		    ?>
-                    <a href='<?= $link . $news->newsId; ?>'>             
-                        <h3><?= $news['name'] ?></h3>
-			</a>
-			<form method="POST">
-		    <input type="hidden" name="deleteNewsId" value="<?= $news['id'] ?>"/>
-		    <input type="submit"  value='удалить'/>
-			</form>
-                    
-                    <p><?= $news['text'] ?></p>
-		    
-		    
-		<?php endforeach; ?>
-		<p>
-		    <a href="Views/newNews.php">
-			<input type="button" name="cabinet_newNews" value="Добавить новость" class="addNews"/>
-		    </a>
-		</p>
-	    </div>
-	<form method="POST">
-            <input type="submit" name="cabinet_logOut" value="Выход" id="output"/>
-        </form>
-         <?php include_once 'Views/Footer.php'; ?> 
+        <header class="w3-light-grey">
+            <form method="POST">
+                <p id="logoTop"><?php echo $loginUser; ?> <input class="w3-button w3-teal" type="submit" name="cabinet_logOut" value="Выход" id="output"/> </p>
+                <hr>
+            </form>
+        </header>
+        <div id="cabinet_allNews" class="w3-container">
+            <form method="POST" class="w3-container">
+                <p>
+                    <label id="myArt">Мои Статьи</label>
+                    <a href="Views/newNews.php">
+                        <input class="w3-button w3-teal addNews" type="button" name="cabinet_newNews" value="+" id="addNews" />
+                    </a>  
+                </p>
+                <?php
+                $db = new DataBase();
+                $user = $db->getUser($loginUser);
+                $news_array = $db->getNewsByLogin($loginUser);
+                foreach ($news_array as $news):
+                    $link = 'http://oct-blog/SingleNews.php?newsId=' . $news['id'];
+                    ?>
+
+                    <div class="w3-panel w3-pale-blue w3-leftbar w3-border-teal" id="title">
+                        <a href='<?= $link . $news->newsId; ?>'
+                           <h3><?= $news['name'] ?></h3>
+                            <input class="w3-button w3-teal" type="submit"  value='x' id="delete"/>
+                        </a>
+                    </div>
+
+                    <input type="hidden" name="deleteNewsId" value="<?= $news['id'] ?>"/>
+                    <!--<input class="w3-button w3-teal" type="submit"  value='удалить'/>-->
+
+
+                <?php endforeach; ?>
+                <input class="w3-button w3-teal addNews" type="button" name="cabinet_newNews" value="+" id="addNews" />
+            </form>
+        </div>
+        <div id="fisrtPage" class="w3-panel w3-pale-blue w3-leftbar w3-border-teal" >
+           <form method="POST" class="w3-container w3-panel w3-pale-blue w3-leftbar w3-border-teal" id="formFirst">
+                <h1 id="logoFirst">Привет, <?php echo $loginUser; ?>! </h1>
+                <p><input class="w3-button w3-teal" type="submit" name="cabinet_logOut" value="Выход" id="outputFirst"/></p>
+            </form> 
+        </div>
+
     </body>
 </html>
